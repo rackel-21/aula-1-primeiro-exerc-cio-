200d24a586262f6e3395d2aa54fbcaed53797398
Rackel <Rackel@DESKTOP-6P2OKKN>
2021-09-15 11:23:21 -0300

    Adicionar arquivos de projeto.

---
+++ aula 1 (primeiro exercício)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula_1__primeiro_exercício_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá,Rackel!");
+        }
+    }
+}
